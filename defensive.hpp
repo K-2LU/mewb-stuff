e432f37a46de2ea210817f957781a6a5fa4827cd
K2LU <rigansust@gmail.com>
2021-09-20 20:22:02 +0600

    another bug fixed for position 0 2

--- defensive.hpp
+++ defensive.hpp
@@ -2,7 +2,7 @@
 #include "updateCPU.hpp"
 
 bool defCPU(char (&table)[3][3], bool (&visited)[3][3], int &emptyCells, int &x, int &y) {
-    int detect = 0, hdetect;
+    int detect = 0, hdetect, hx, hy;
     bool hphen = false;
 
     // horizontal sweep
@@ -36,24 +36,25 @@ bool defCPU(char (&table)[3][3], bool (&visited)[3][3], int &emptyCells, int &x,
     }
 
     // for pos: 00 11 22
-    for (size_t i = 0, j = 0; i, j < 3; i++, j++)
+    for (size_t i = 0, j = 0; i < 3 && j < 3; i++, j++)
     {
         if(table[i][j] == 'X')  detect += 1;
-        if(table[i][j] == '-')  hphen = true;
+        if(table[i][j] == '-')  hphen = true, hx = i, hy = j;
         
         if(detect==2 && hphen == true){
-            update(table, visited, i, j, emptyCells);
+            update(table, visited, hx, hy, emptyCells);
             return true;
         }
+        detect = 0, hphen = false;
     }
 
-    // for pos: 02 11 20
-    for (size_t i = 0, j = 2; i < 3, j >= 0; i++, j--)  {
-        if(table[i][j] == 'X')  detect += 1;
-        if(table[i][j] == '-')  hphen = true;
+    // for pos: 02 11 20 
+    for (size_t i = 0, j = 2; i < 3 && j >= 0; i++, j--)  {
+        if(table[j][i] == 'X')  detect += 1;
+        if(table[j][i] == '-')  hphen = true, hx = j, hy = i;
         
         if(detect==2 && hphen == true){
-            update(table, visited, i, j, emptyCells);
+            update(table, visited, hx, hy, emptyCells);
             return true;
         }
     }
